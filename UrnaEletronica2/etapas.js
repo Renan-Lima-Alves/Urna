f9cf7b655dcea9fe045ce2f0cb4bc56734ed1be3
Renan-Lima-Alves <122834352+Renan-Lima-Alves@users.noreply.github.com>
2023-01-25 19:45:53 -0300

    Urna eletrônica 2.0

---
+++ UrnaEletronica2/etapas.js
@@ -0,0 +1,35 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '13171',
+                nome: 'Lula',
+                partido: 'PT',
+                fotos: [
+                    {url: 'lula.jfif', legenda: 'Vereador'}
+                ]
+            },          
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '10',
+                nome: 'Chapolin',
+                partido: 'XYZ',
+                vice: 'Chaves',
+                fotos: [
+                    {url: 'chapolin.jpg', legenda: 'Prefeito'},
+                    {url: 'chaves.jpeg', legenda: 'Vice-Prefeito', small: true}
+
+                ]
+            },          
+        ]
+    }
+    
+]
